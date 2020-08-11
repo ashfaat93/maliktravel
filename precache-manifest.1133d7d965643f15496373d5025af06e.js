b3e57ea25490fd7ac2f1b56c5ff828a1f1ec40df
Ali <ashfaat@gmail.com>
2020-08-11 12:06:31 +0100

    Updates

---
+++ precache-manifest.1133d7d965643f15496373d5025af06e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0a334e3a1f8d5e186879f2a1b52c0a3",
+    "url": "/travelagency/index.html"
+  },
+  {
+    "revision": "ae680d282352a274fef6",
+    "url": "/travelagency/static/css/main.99d9d52b.chunk.css"
+  },
+  {
+    "revision": "e8bbbd57bd9624bd87b2",
+    "url": "/travelagency/static/js/2.75751ea7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/travelagency/static/js/2.75751ea7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae680d282352a274fef6",
+    "url": "/travelagency/static/js/main.79cd9598.chunk.js"
+  },
+  {
+    "revision": "ff4ba576100925437162",
+    "url": "/travelagency/static/js/runtime-main.5ccc76a1.js"
+  }
+]);
